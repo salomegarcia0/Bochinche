880bf7b359289cf64be7b3c63215ccadd9d67e99
salomegarcia0 <salome.garcia@correo.unimet.edu.ve>
2026-01-27 17:50:14 -0400

    feat : Integración inicial de Firebase
    - Se añadieron las dependencias necesarias.
    - Se incluye el archivo de configuración: firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:bochinche_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future <void> main() async {
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  debugPrint("Conectado a Firebase: ${Firebase.app().name}");
   runApp(const MyApp());
 }
 
