f59fd94975c241a71961a861bde5448e7830a224
Diegoafkx <diego.arreaza@correo.unimet.edu.ve>
2026-03-09 18:04:46 -0400

    [Fix]: Interface fixes

--- lib/features/map/BuscadorEventoMapa.dart
+++ lib/features/map/BuscadorEventoMapa.dart
@@ -48,42 +48,14 @@ class _BuscadorEventoMapaState extends State<BuscadorEventoMapa> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-      decoration: BoxDecoration(
-        color: PrimaryBackGroundPurple,
-        borderRadius: BorderRadius.circular(50.0),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Container()],
-          ),
-          buscador(context),
-        ],
-      ),
-    );
-  }
-
-  Widget filtro(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
-      child: FilledButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PublicEventsScreen()),
-          );
-        },
-        style: FilledButton.styleFrom(
-          backgroundColor: PrimaryPurple,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+        decoration: BoxDecoration(
+          color: PrimaryBackGroundPurple,
+          borderRadius: BorderRadius.circular(50.0),
         ),
-        child: const Text('Filtros', style: TextStyle(color: Colors.white)),
+        child: buscador(context),
       ),
     );
   }
@@ -117,16 +89,6 @@ class _BuscadorEventoMapaState extends State<BuscadorEventoMapa> {
             },
             decoration: InputDecoration(
               prefixIcon: const Icon(Icons.search, color: PrimaryPurple),
-              /*suffixIcon: _showPasteButton
-                  ? IconButton(
-                      icon: const Icon(
-                        Icons.content_paste,
-                        color: PrimaryPurple,
-                      ),
-                      onPressed: _pasteFromClipboard,
-                      tooltip: 'Pegar código',
-                    )
-                  : null,*/
               hintText: 'Buscar evento o código privado',
               border: InputBorder.none,
               contentPadding: const EdgeInsets.symmetric(
